83999db428974ff160ca314c130fe5a9352bbda2
Behinding978 <156109664+BehindingTheRealOne@users.noreply.github.com>
2025-02-21 12:11:57 -0500

    Update script.js

--- 94199925-7986-498e-ab8e-8809b8b6133e.html/script.js
+++ 94199925-7986-498e-ab8e-8809b8b6133e.html/script.js
@@ -71,3 +71,25 @@ function m2() {
     var chromenetwrk = window.open("chrome://network", "_blank");
     if (chromenetwrk) chromenetwrk.focus();
 }
+
+function abtblank() {
+  javascript: (function() {
+    var url = prompt("MADE BY UNBLOCKEDGAMESHTPS", "Insert link here.");
+    var urlObj = new window.URL(window.location.href);
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.referrerpolicy = "no-referrer";
+    iframe.allow = "fullscreen";
+    iframe.src = url.toString();
+    win.document.body.appendChild(iframe);
+    var script = win.document.createElement("script");
+    script.src = "https://3kh0.github.io/js/main.js";
+    win.document.body.appendChild(script);
+})();
+}
