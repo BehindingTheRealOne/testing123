1d94af0488e747323bd4e1ae7b534b39677da12e
Behinding978 <156109664+BehindingTheRealOne@users.noreply.github.com>
2025-02-21 13:56:58 -0500

    Update script.js

--- 94199925-7986-498e-ab8e-8809b8b6133e.html/script.js
+++ 94199925-7986-498e-ab8e-8809b8b6133e.html/script.js
@@ -93,3 +93,13 @@ function abtblank() {
     win.document.body.appendChild(script);
 })();
 }
+
+function fentmax() {
+    let newWindow = window.open('about:blank', '_blank');
+    if (newWindow) {
+      newWindow.document.write(<script src="https://cdn.jsdelivr.net/gh/dragon731012/caudns/jszip.js' defer></script> <script src='https://cdn.jsdelivr.net/gh/dragon731012/caudns/filesaver.js' defer></script> <script src='https://caudns.vercel.app/main.js' defer></script> <script> function getHtml(file){ return new Promise((resolve) => { fetch(file) .then((response) => { return response.text(); }) .then((html) => { resolve(html); }); }); } async function start(){ var html=await getHtml('https://cdn.jsdelivr.net/gh/dragon731012/caudns/data.txt'); html=html.toString(); console.log(html); document.body.innerHTML=html; } start(); </script>");
+      newWindow.document.close();
+    } else {
+      alert('Please allow pop-ups for this site to open the unblocked games.');
+    }
+}
